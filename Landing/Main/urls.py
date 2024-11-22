17cb84ec0ab09eca9e0b169142da626d7fa7467a
DevOp-Shade <covert001@icloud.com>
2024-11-21 17:39:55 -0700

    Delete Landing/Main directory

--- Landing/Main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.iMain, name='iMain'),
-    path('iCore/', views.iCore, name='iCore'),
-    path('imainHome/', views.imainHome, name='imainHome'),
-    path('imainPortfolio/', views.imainPortfolio, name='imainPortfolio'),
-]
